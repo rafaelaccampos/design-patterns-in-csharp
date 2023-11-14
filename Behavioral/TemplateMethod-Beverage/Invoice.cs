c5ac54459b092ba8b956f94a07d5aba5dd917c64
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-11-14 06:21:42 -0300

    feat: Add Invoice clas and Template Method tests

--- Behavioral/TemplateMethod-Beverage/Invoice.cs
+++ Behavioral/TemplateMethod-Beverage/Invoice.cs
@@ -2,6 +2,23 @@
 {
     public class Invoice
     {
+        private readonly IList<Item> _items;
 
+        public Invoice()
+        {
+            _items = new List<Item>();
+        }
+
+        public void Add(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public decimal GetTaxes()
+        {
+            return _items.Where(item => item is TaxItem)
+                .Cast<TaxItem>()
+                .Sum(taxItem => taxItem.CalculateTax());
+        }
     }
 }
