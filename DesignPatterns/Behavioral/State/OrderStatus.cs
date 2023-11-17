96cda03f76be2dfbb09b1facd7e0292c3016118a
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2023-11-16 23:04:16 -0300

    feat: Initial implementation of state

---
+++ DesignPatterns/Behavioral/State/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Behavioral.State
+{
+    public abstract class OrderStatus
+    {
+        private readonly Order _order;
+
+        public OrderStatus()
+        {
+            _order = new Order();
+        }
+
+        public abstract string Value { get; set; }
+
+        public abstract void Confirm();
+
+        public abstract void Cancel();
+    }
+}
