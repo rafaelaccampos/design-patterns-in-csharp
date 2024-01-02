1c7427d9473a8df4776f42aba4a8b9fc4b02a00d
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-01-01 23:23:23 -0300

    Update READ.MD with adapter description

--- DesignPatterns/Structural/Adapter/Adapter-Transaction/PayPalTransactionAdapter.cs
+++ DesignPatterns/Structural/Adapter/Adapter-Transaction/PayPalTransactionAdapter.cs
@@ -15,6 +15,7 @@
 
         public string Status { get; private set; }
 
+//TODO: I can use dictionary here instead of switch
         public string ConvertStatus(string status)
         {
             switch (status)
