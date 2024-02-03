98be6cdec6e427e3746071507baa40da754ad6f8
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-02-02 21:35:39 -0300

    Add singleton implementation

---
+++ DesignPatterns/Creational/Singleton/BusinessHours.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    public class BusinessHours
+    {
+        private BusinessHours(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        private static BusinessHours _instance;
+
+        public DateTime StartTime { get; private set; }
+
+        public DateTime EndTime { get; private set; }
+
+        public static BusinessHours GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new BusinessHours(
+                    new DateTime(1, 1, 1, new Random().Next(0, 10), 0, 0),
+                    new DateTime(1, 1, 1, new Random().Next(18, 24), 0, 0)
+                    );
+            }
+
+            return _instance;
+        }
+    }
+}
