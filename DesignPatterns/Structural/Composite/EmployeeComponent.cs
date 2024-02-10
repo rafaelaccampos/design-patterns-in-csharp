c04485e81fe66ca128f8552d910fd80fef4af8f8
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-02-09 23:17:51 -0300

    feat: Add composite design pattern

---
+++ DesignPatterns/Structural/Composite/EmployeeComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns.Structural.Composite
+{
+    public abstract class EmployeeComponent
+    {
+        protected EmployeeComponent(
+            string name,
+            string role, 
+            decimal expenses)
+        {
+            Name = name;
+            Role = role;
+            Expenses = expenses;
+        }
+
+        public string Name { get; private set; }
+
+        public string Role { get; private set; }
+
+        public decimal Expenses { get; private set; }
+
+        public abstract decimal GetExpenses();
+    }
+}
---
+++ DesignPatterns/Structural/Composite/ManagerComposite.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatterns.Structural.Composite
+{
+    public class ManagerComposite : EmployeeComponent
+    {
+        private readonly IList<EmployeeComponent> _children;
+
+        public ManagerComposite(string name, string role, decimal expenses) 
+            : base(name, role, expenses)
+        {
+            _children = new List<EmployeeComponent>();
+        }
+
+        public override decimal GetExpenses()
+        {
+            return _children.Sum(c => c.GetExpenses()) + Expenses;
+        }
+
+        public void Add(EmployeeComponent component)
+        {
+            _children.Add(component);
+        }
+
+        public void Remove(EmployeeComponent component)
+        {
+            _children.Remove(component);
+        }
+    }
+}
