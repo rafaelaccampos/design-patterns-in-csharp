7060274f215ed8e4d9630c35b2ac1f8ed593e452
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-02-16 23:27:04 -0300

    Update README.MD

---
+++ DesignPatterns/Structural/Proxy/CustomerRepositoryProxy.cs
@@ -0,0 +1,37 @@
+﻿namespace DesignPatterns.Structural.Proxy
+{
+    public class CustomerRepositoryProxy
+    {
+        public CustomerRepositoryProxy(
+            CustomerRepository customerRepository,
+            IMemoryCache cache,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _customerRepository = customerRepository;
+            _cache = cache;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IList<Customer?> GetBlockedUsers()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            if(httpContext == null)
+            {
+                return null;
+            }
+
+            if (httpContext.Request.Headers["x-role"] != "admin")
+            {
+                return null;
+            }
+
+            var blockedCustomers = _cache.GetOrCreate("blocked-customers", c =>
+            {
+                return _customerRepository.GetBlockedUsers();
+            });
+
+            return blockedCustomers;
+        }
+    }
+}
