ed768bc457c16e9fe8da4332842837b379a4467a
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-06-01 20:21:01 -0300

    feat: Add Prototype design pattern

---
+++ DesignPatterns/Creational/Prototype/Car.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatterns.Creational.Prototype
+{
+    public class Car : IVehiclePrototype
+    {
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public int Seats { get; private set; }
+
+        public Car(string model, string color, int seats)
+        {
+            Model = model;
+            Color = color;
+            Seats = seats;
+        }
+
+        public IVehiclePrototype Clone()
+        {
+            return (IVehiclePrototype)this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"Car: {Model}, Color: {Color}, Seats: {Seats}";
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Prototype/IVehiclePrototype.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns.Creational.Prototype
+{
+    public interface IVehiclePrototype
+    {
+        IVehiclePrototype Clone();
+    }
+}
