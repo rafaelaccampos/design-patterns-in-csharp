a3dc2c17eddd5f2b964786943226cca68e51b8cb
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-07-15 09:44:45 -0300

    feat: Add credit card service test for factory method

--- DesignPatterns.Tests/Factory Method/PaymentServiceTests.cs
+++ DesignPatterns.Tests/Factory Method/PaymentServiceTests.cs
@@ -6,7 +6,7 @@ namespace DesignPatterns.Tests
     public class PaymentServiceFactoryTests
     {
         [Test]
-        public void Create_ShouldProcessOrderWithPaymentSlipService()
+        public void ShouldProcessOrderWithPaymentSlipService()
         {
             var factory = new PaymentServiceFactory();
             var order = new Order(factory);
@@ -23,6 +23,26 @@ namespace DesignPatterns.Tests
             var paymentSlip = service.Process(orderItem);
 
             paymentSlip.Should().Be("Dados do boleto");
+        }
+
+        [Test]
+        public void ShouldProcesssOrderWithCreditCardService()
+        {
+            var factory = new PaymentServiceFactory();
+            var order = new Order(factory);
+            var orderItem = new OrderItem
+            {
+                ProductId = Guid.NewGuid(),
+                Quantity = 1,
+                PaymentMethod = PaymentMethod.CreditCard
+            };
+
+            order.Create(orderItem);
+
+            var service = factory.GetService(orderItem.PaymentMethod);
+            var creditCard = service.Process(orderItem);
+
+            creditCard.Should().Be("Transação aprovada!");
         }
     }
 }
