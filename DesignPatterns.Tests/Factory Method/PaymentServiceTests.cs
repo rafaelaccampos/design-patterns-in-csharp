6754f1d260d6fb1945156eb61b6a73fa778d5cb9
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-07-14 22:57:40 -0300

    Add paymentslip test to factory method design pattern

--- DesignPatterns.Tests/Factory Method/PaymentServiceTests.cs
+++ DesignPatterns.Tests/Factory Method/PaymentServiceTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DesignPatterns.Creational.Factory_Method;
+using FluentAssertions;
 
-namespace DesignPatterns.Tests.Factory_Method
+namespace DesignPatterns.Tests
 {
-    internal class PaymentServiceTests
+    public class PaymentServiceFactoryTests
     {
+        [Test]
+        public void Create_ShouldProcessOrderWithPaymentSlipService()
+        {
+            var factory = new PaymentServiceFactory();
+            var order = new Order(factory);
+            var orderItem = new OrderItem
+            {
+                ProductId = Guid.NewGuid(),
+                Quantity = 1,
+                PaymentMethod = PaymentMethod.PaymentSlip
+            };
+
+            order.Create(orderItem);
+
+            var service = factory.GetService(orderItem.PaymentMethod);
+            var paymentSlip = service.Process(orderItem);
+
+            paymentSlip.Should().Be("Dados do boleto");
+        }
     }
 }
