fff8e8946421032c0b95af0f750a0b39476b26ea
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-07-27 14:07:14 -0300

    docs: Update README.MD

--- DesignPatterns.Tests/Prototype/CarTests.cs
+++ DesignPatterns.Tests/Prototype/CarTests.cs
@@ -11,25 +11,25 @@ namespace DesignPatterns.Tests.Prototype
         {
             var car = new Car("Toyota Camry", "Red", 5);
 
-            var clonedCar = (Car)car.Clone();
+            var clonedCar = car.Clone() as Car;
 
-            using(new AssertionScope())
+            using (new AssertionScope())
             {
-                car.Model.Should().Be(clonedCar.Model);
+                car.Model.Should().Be(clonedCar!.Model);
                 car.Color.Should().Be(clonedCar.Color);
                 car.Seats.Should().Be(clonedCar.Seats);
             }
         }
 
         [Test]
-        public void ShouldBeAbleToCloneAndModifyWithoutAffectTheOriginalCar()
+        public void ShouldBeAbleToCloneAndModifyWithoutAffectingTheOriginalCar()
         {
             var car = new Car("Toyota Camry", "Red", 5);
 
-            var clonedCar = (Car)car.Clone();
-            clonedCar.Color = "Blue";
+            var clonedCar = car.Clone() as Car;
+            clonedCar!.Color = "Blue";
 
-            using(new AssertionScope())
+            using (new AssertionScope())
             {
                 car.Color.Should().Be("Red");
                 clonedCar.Color.Should().Be("Blue");
