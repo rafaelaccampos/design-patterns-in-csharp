d66e7d508ab0ab5d4e99ff0e407d1a475a8e5ef2
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-08-03 14:44:11 -0300

    docs: Update README.MD

--- DesignPatterns.Tests/Facade/EcommerceFacadeTests.cs
+++ DesignPatterns.Tests/Facade/EcommerceFacadeTests.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPatterns.Structural.Facade;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using NSubstitute;
 
 namespace DesignPatterns.Tests.Facade
@@ -30,6 +31,31 @@ namespace DesignPatterns.Tests.Facade
             var placeOrder = _ecommerceFacade.PlaceOrder("Produto123", 2, "1233331313", "Rua Horacio");
 
             placeOrder.Should().BeFalse();
-        } 
+        }
+
+        [Test]
+        public void ShouldReturnTrueWhenAllServicesSucceed()
+        {
+            var inventoryService = Substitute.For<IInventoryService>();
+            var paymentService = Substitute.For<IPaymentService>();
+            var shippingService = Substitute.For<IShippingService>();
+
+            inventoryService.CheckStock(Arg.Any<string>()).Returns(true);
+            paymentService.ProcessPayment(Arg.Any<string>(), Arg.Any<decimal>()).Returns(true);
+            shippingService.CalculateShippingCost(Arg.Any<string>()).Returns(10.0m);
+            shippingService.GenerateShippingLabel(Arg.Any<string>()).Returns("EtiquetaDeEnvio2024");
+
+            var facade = new EcommerceFacade(paymentService, shippingService, inventoryService);
+
+            var placeOrder = facade.PlaceOrder("productId", 1, "1234567890", "address");
+
+            using (new AssertionScope())
+            {
+                placeOrder.Should().BeTrue();
+                inventoryService.Received(1).CheckStock("productId");
+                paymentService.Received(1).ProcessPayment("1234567890", 100);
+                shippingService.Received(1).GenerateShippingLabel("address");
+            }
+        }
     }
 }
